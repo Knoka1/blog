9533652fa3adc7285d470261e3ea728ac708efbc
Knoka1 <knoka1@hotmail.com>
2024-05-21 20:06:42 -0300

    fix: Adds revalidation so posts are updated whenever a new one is created

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 const fetchPosts = async (): Promise<Post[]> => {
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch("http://localhost:3000/api/posts", {
+    next: { revalidate: 1 },
+  });
   if (!res.ok) {
     throw new Error("Failed to fetch posts");
   }
