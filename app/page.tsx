ffcad4dc238bc425a0bcb3f490da6b1630e4ef3b
Knoka1 <knoka1@hotmail.com>
2024-05-21 16:59:03 -0300

    feature: Updates Home with title and subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return <section>b</section>;
+  // const displayPosts = posts;
+  return (
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
+          <p className="text-xl text-muted-foreground">
+            All things oil and tech
+          </p>
+        </div>
+      </div>
+      <hr className="mt-8" />
+    </div>
+  );
 }
