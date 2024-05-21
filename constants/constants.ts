c0b05b9b7a66ff70b08a26ed9fbb51e64cdae953
Knoka1 <knoka1@hotmail.com>
2024-05-21 19:52:31 -0300

    fix: Deletes unnecessary constants

--- constants/constants.ts
+++ constants/constants.ts
@@ -8,26 +8,5 @@ export const generalSite = {
     github: "https://github.com/Knoka1",
   },
 };
-export const sidebarLinks = [
-  {
-    imgURL: "/icons/home.svg",
-    route: "/",
-    label: "Home",
-  },
-  {
-    imgURL: "/icons/dollar-circle.svg",
-    route: "/my-banks",
-    label: "My Banks",
-  },
-  {
-    imgURL: "/icons/transaction.svg",
-    route: "/transaction-history",
-    label: "Transaction History",
-  },
-  {
-    imgURL: "/icons/money-send.svg",
-    route: "/payment-transfer",
-    label: "Transfer Funds",
-  },
-];
+
 export type GeneralSite = typeof generalSite;
