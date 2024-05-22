f56fa59d3dca3e9059173f8c9b5770a3fbcefc09
Knoka1 <knoka1@hotmail.com>
2024-05-21 22:07:56 -0300

    fix: Updates Author and link to link to Alec do Couto

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,10 +2,11 @@ export const generalSite = {
   name: "OilBlog",
   url: "https://example.com",
   description: "Your blog for figuring out oil",
-  author: "D'Bragas",
+  author: "Alec do Couto",
   links: {
     linkedin: "https://www.linkedin.com/in/alecdocouto/",
     github: "https://github.com/Knoka1",
+    personalSite: "https://alecdocouto.com",
   },
 };
 
