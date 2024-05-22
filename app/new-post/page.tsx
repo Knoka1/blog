69c149f362efc562760e51d9d81e1d8a1b40bf12
Knoka1 <knoka1@hotmail.com>
2024-05-22 14:52:26 -0300

    feature: Adds NewPostForm page and skeleton component

--- app/new-post/page.tsx
+++ app/new-post/page.tsx
@@ -1,7 +1,12 @@
+import NewPostForm from "@/components/NewPostForm";
 import React from "react";
 
 const NewPost = () => {
-  return <div>NewPost</div>;
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <NewPostForm />
+    </section>
+  );
 };
 
 export default NewPost;
